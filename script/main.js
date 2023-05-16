418f3ea1383ce19433fda1f379897128abf86491
St-Jousef <Joseph_nkwocha>
2023-05-16 13:01:51 +0100

    80% done

--- script/main.js
+++ script/main.js
@@ -38,35 +38,42 @@ const firstTitle = document.createElement("p");
 
 // Add event listeners to the buttons
 addButton.addEventListener("click", function () {
-  // Increment the score by 3 (but don't let it go beyond 100)
+  // Increment the score by 10 (but don't let it go beyond 100)
   // var score = (scoreElement.innerText);
 
   if (score < 200) {
     score += 10;
     scoreElement.innerText = score;
-    if (score == 50) {
-      // Update the image to the happy face
+
+    if (score >= 0 && score < 50) {
+      // Update the score with text with the default emoji
+      firstTitle.innerHTML = `you have scored ${score} point..  This is awful !!`;
+      paragraph.appendChild(firstTitle);
+    }
+
+    if (score >= 50 && score < 100) {
+      // Update the emoji to confuse
       imageElement.src = "images/confuse.png";
       firstTitle.innerHTML = `you have scored ${score} point..  This is not good !!`;
       paragraph.appendChild(firstTitle);
     }
 
-    if (score == 100) {
-      // Update the image to the happy face
+    if (score >= 100 && score < 150) {
+      // Update the emoji to neutral
       imageElement.src = "images/neutral.png";
       firstTitle.innerHTML = `you have scored ${score} point.. You are an average performer. You could do better !!`;
       paragraph.appendChild(firstTitle);
     }
 
-    if (score == 150) {
-      // Update the image to the happy face
+    if (score >= 150 && score < 200) {
+      // Update the emoji to happy
       imageElement.src = "images/happy.png";
       firstTitle.innerHTML = `you have scored ${score} point.. Slightly above average. Keep pushing!!`;
       paragraph.appendChild(firstTitle);
     }
 
-    if (score == 200) {
-      // Update the image to the happy face
+    if (score >= 200) {
+      // Update the emoji to extreme happy
       imageElement.src = "images/extremehappy.png";
       firstTitle.innerHTML = `you have scored ${score} point .. wow what a performer!!! You are the best..`;
       paragraph.appendChild(firstTitle);
@@ -76,35 +83,41 @@ addButton.addEventListener("click", function () {
 });
 
 subtractButton.addEventListener("click", function () {
-  // Decrement the score by 3 (but don't let it go below 0)
+  // Decrement the score by 10 (but don't let it go below 0)
   //   var score = parseInt(scoreElement.innerText);
   if (score > 0) {
     score -= 10;
     scoreElement.innerText = score;
 
-    if (score == 50) {
-      // Update the image to the happy face
+    if (score == 0) {
+      // Update the emoji face and the score
+      firstTitle.innerHTML = `you have scored ${score} point..  This is not good !!`;
+      paragraph.appendChild(firstTitle);
+    }
+
+    if (score <= 50 && score > 0) {
+      // Update the face and score
       imageElement.src = "images/sad.png";
       firstTitle.innerHTML = `you have scored ${score} point..  This is not good !!`;
       paragraph.appendChild(firstTitle);
     }
 
-    if (score == 100) {
-      // Update the image to the happy face
+    if (score <= 100 && score > 50) {
+      // Update the emoji face and score
       imageElement.src = "images/neutral.png";
       firstTitle.innerHTML = `you have scored ${score} point.. You are an average performer. You could do better !!`;
       paragraph.appendChild(firstTitle);
     }
 
-    if (score == 150) {
-      // Update the image to the happy face
+    if (score <= 150 && score > 100) {
+      // Update the emoji and score
       imageElement.src = "images/happy.png";
       firstTitle.innerHTML = `you have scored ${score} point.. Slightly above average. Keep pushing!!`;
       paragraph.appendChild(firstTitle);
     }
 
-    if (score == 200) {
-      // Update the image to the happy face
+    if (score <= 200 && score > 150) {
+      // Update the emoji and score
       imageElement.src = "images/extremehappy.png";
       firstTitle.innerHTML = `you have scored ${score} point .. wow what a performer!!! You are the best..`;
       paragraph.appendChild(firstTitle);
